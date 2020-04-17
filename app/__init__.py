2c20aea6c8e9f8fe1b3dbd1e5768ac15aa966401
Paulo Tinoco <paulovictortinoco@gmail.com>
2020-04-17 08:00:37 -0300

    Alterando retorno de mensagens

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,22 @@
+import time
 from .Stackoverflow import Stackoverflow
+
+
+def create_app():
+    stack = Stackoverflow()
+    search = input("Enter the search criteria : ") 
+    
+    response_stackoverflow = stack.searchDiscussion(search)
+    response_items = response_stackoverflow['data']['items']
+    
+    if len(response_items) == 0:
+        print('your search returned no results')
+    
+    for question in response_items:
+        if question['is_answered'] is True:
+            response = 'Title: {}\n'.format(question['title'])
+            response += 'Link: {}\n'.format(question['link'])
+            response += 'Score: {}\n'.format(question['score'])
+
+        print(response)
+        time.sleep(1)
--- run.py
+++ run.py
@@ -1,19 +1,5 @@
-import time
-from app import Stackoverflow
+from app import create_app
 
 
 if __name__ == "__main__":
-    stack = Stackoverflow()
-    search = input("Enter the search criteria : ") 
-    
-    result = stack.searchDiscussion(search)
-
-    for question in result['data']['items']:
-        print('\n\n---------------------------------------------\n')
-        print('Title: {}'.format(question['title']))
-        print('Score: {}'.format(question['score']))
-        print('Is Answered: {}'.format(question['is_answered']))
-        print('Link: {}'.format(question['link']))
-        time.sleep(1)
-
-    print('---------------------------------------------\n\n')
+    create_app()
